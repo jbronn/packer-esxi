7fd299c8f4d06a952c603f7309ca9c69bcfa3559
Justin Bronn <jbronn@gmail.com>
2016-02-11 20:30:34 -0800

    Fix prompt.

--- scripts/openbsd/bash.sh
+++ scripts/openbsd/bash.sh
@@ -21,11 +21,8 @@ alias sudo='doas'
 # Minimalist path (e.g., no X11, games) for server systems.
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin:/usr/libexec
 
-# Define default prompt to <username>@<hostname>:<path><"($|#) ">
-# and print '#' for user "root" and '$' for normal users.
-PS1='${LOGNAME}@$(/bin/hostname -s):$(
-    [[ "${LOGNAME}" == "root" ]] && printf "%s" "${PWD/${HOME}/~}# " ||
-    printf "%s" "${PWD//${HOME}/~}\$ ")'
+# Define default prompt.
+PS1='\u@\h:\w\$ '
 EOF
 
 echo 'test -r ~/.bashrc && source ~/.bashrc' > ~/.bash_profile
