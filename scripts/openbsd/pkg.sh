b5a67100d610a697715ceff1ba88db2f6ec3840d
Justin Bronn <jbronn@gmail.com>
2016-02-11 20:31:02 -0800

    Allow customization of the OpenBSD packaging mirror.

--- scripts/openbsd/pkg.sh
+++ scripts/openbsd/pkg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-export PKG_PATH=http://ftp3.usa.openbsd.org/pub/OpenBSD/`uname -r`/packages/`arch -s`/
+export PKG_PATH=$PACKAGE_MIRROR/`uname -r`/packages/`arch -s`/
 echo "installpath=${PKG_PATH}" > /tmp/pkg.conf
 doas chown root:wheel /tmp/pkg.conf
 doas mv /tmp/pkg.conf /etc/pkg.conf
